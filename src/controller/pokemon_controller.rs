bc0ffe7120563c59a150b115d71db30de7cf8c95
Eugen Sawitzki <e.sawitzki@reply.de>
2023-03-13 16:00:02 +0100

    change type to &str

--- src/controller/pokemon_controller.rs
+++ src/controller/pokemon_controller.rs
@@ -18,7 +18,7 @@ pub async fn get_pokemon_by_id(db: web::Data<Pool>, poke_id: web::Path<String>) 
         HttpResponse::Ok().body(result.get(0).unwrap().to_owned())
     } else {
         // fetch pokemon
-        let pokemon_response = fetch_pokemon_for_id(poke_id.to_string()).await;
+        let pokemon_response = fetch_pokemon_for_id(poke_id.as_str()).await;
 
         if pokemon_response.is_ok() {
             let pokemon = pokemon_response.unwrap();
@@ -30,8 +30,8 @@ pub async fn get_pokemon_by_id(db: web::Data<Pool>, poke_id: web::Path<String>) 
     }
 }
 
-async fn fetch_pokemon_for_id(poke_id: String) -> Result<Pokemon, Box<dyn Error>> {
-    let pokemon_response = reqwest::get("https://pokeapi.co/api/v2/pokemon/".to_string() + poke_id.as_str()).await?.json::<Pokemon>().await?;
+async fn fetch_pokemon_for_id(poke_id: &str) -> Result<Pokemon, Box<dyn Error>> {
+    let pokemon_response = reqwest::get("https://pokeapi.co/api/v2/pokemon/".to_string() + poke_id).await?.json::<Pokemon>().await?;
 
     Ok(pokemon_response)
 }
