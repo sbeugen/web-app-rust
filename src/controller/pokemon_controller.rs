01863daff7a04532ce86cc43b8ccda5385879bc4
Eugen Sawitzki <e.sawitzki@reply.de>
2023-03-16 15:40:39 +0100

    use pattern matching

--- src/controller/pokemon_controller.rs
+++ src/controller/pokemon_controller.rs
@@ -1,6 +1,6 @@
-use std::error::Error;
 use actix_web::{get, web, HttpResponse, Responder};
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
+use std::error::Error;
 
 use crate::db::Pool;
 use crate::repository::pokemon_repository;
@@ -13,25 +13,38 @@ struct Pokemon {
 
 #[get("/pokemon/{poke_id}")]
 pub async fn get_pokemon_by_id(db: web::Data<Pool>, poke_id: web::Path<String>) -> impl Responder {
-    let result = pokemon_repository::get_by_id_if_exists(&db, poke_id.to_string()).await.unwrap();
+    let result = pokemon_repository::get_by_id_if_exists(&db, poke_id.to_string()).await;
+    let result = match result {
+        Ok(names) => names,
+        Err(_) => vec![],
+    };
+
     if result.len() > 0 {
         HttpResponse::Ok().body(result.get(0).unwrap().to_owned())
     } else {
         // fetch pokemon
         let pokemon_response = fetch_pokemon_for_id(poke_id.as_str()).await;
 
-        if pokemon_response.is_ok() {
-            let pokemon = pokemon_response.unwrap();
-            let _store_result = pokemon_repository::add_cache_entry(&db, pokemon.name.clone(), pokemon.id.to_string()).await;
-            HttpResponse::Ok().body(pokemon.name)
-        } else {
-            HttpResponse::InternalServerError().body("Error")
+        match pokemon_response {
+            Ok(pokemon) => {
+                let _store_result = pokemon_repository::add_cache_entry(
+                    &db,
+                    pokemon.name.clone(),
+                    pokemon.id.to_string(),
+                )
+                .await;
+                HttpResponse::Ok().body(pokemon.name)
+            }
+            Err(_) => HttpResponse::InternalServerError().body("Error"),
         }
     }
 }
 
 async fn fetch_pokemon_for_id(poke_id: &str) -> Result<Pokemon, Box<dyn Error>> {
-    let pokemon_response = reqwest::get("https://pokeapi.co/api/v2/pokemon/".to_string() + poke_id).await?.json::<Pokemon>().await?;
+    let pokemon_response = reqwest::get("https://pokeapi.co/api/v2/pokemon/".to_string() + poke_id)
+        .await?
+        .json::<Pokemon>()
+        .await?;
 
     Ok(pokemon_response)
 }
